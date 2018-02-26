66f75dfaccd8435869817f61858d83f452fa7a76
Captain-Galak <essebee@gmail.com>
2018-02-26 11:49:11 +0100

    update CSS page produits

--- produit_view.php
+++ produit_view.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,13 +12,15 @@
 	<!-- CSS Vendor -->
 	<link rel="stylesheet" type="text/css" href="css/normalize.css" media="all" />
 	<!-- CSS Perso -->
-	<link rel="stylesheet" type="text/css" href="css/DMFstyle.css" media="screen" />
+	<link rel="stylesheet" type="text/css" href="css/style.css?timestamp=<?php echo time() ?> " media="screen" />
+	<script
+	src="http://code.jquery.com/jquery-3.3.1.js"
+	integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+	crossorigin="anonymous"></script>
+	<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+
 	<?php include 'produit.php'; ?>
-<script
-  src="http://code.jquery.com/jquery-3.3.1.js"
-  integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
-  crossorigin="anonymous"></script>
-  <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+
 
 
 
@@ -28,25 +31,40 @@
 
 										<!-- HEADER -->
 	<header>
-	<?php include('header_view.php') ?> 
+	<?php include 'header_view.php' ?> 
 	</header>
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-md-4 criterenav sticky">
+				<div class="col-md-12">
+				<form method="get" action="produit_filtre_view.php">
+					<div class="text-center">
+						Trier les produits par critères :<br />
+						<br />
+					</div>	
+		
 
-		<form method="get" action="produit_filtre_view.php">
-			<p>
-				Trier les produits par critères :<br /><br />
-			</p>										<!-- MAIN -->
+		
+				<!-- MAIN -->
 		<?php 
 
 		$req = afficher_filtre();
 					while ($donnees = $req->fetch()) { //affiche les réponses de la requête jusqu'au dernier résultat
-						?> <p>
+						?>
+					<div class="form-check">
 						<input type="checkbox" name="critere[]" value="<?php echo $donnees['idcritere']?>" id="<?php echo $donnees['idcritere']?>" /> <label for="<?php echo $donnees['idcritere']?>"><?php echo $donnees['nom_critere']?></label> <!-- on donne le même name à tous les champs, ce nom est un tableau critere. On modifie la value par le nom du critere qu'on veut afficher --> 
-						</p>
+					</div>
+						
 					<?php } ?>
 
+			
+	
+				<input  class="btn btn-custom3" type="submit" value="Envoyer" />
+			</div>
+			</div> <!-- FIN Col-md -->
 
 
-				<input type="submit" value="Envoyer" />
+			<div class=" cardsize col-xs-12 col-md-8">
 				
 		<?php
 		$req = afficher_produit();
@@ -54,35 +72,44 @@
 
 		while ($donnees = $req->fetch()) { //affiche les réponses de la requête jusqu'au dernier résultat
 		?>
-			<p>
-			 Chaussure <?php echo $donnees['nom_produit']; ?>, de type <?php echo $donnees['type']; ?><br />
-			 Vous la trouverez en suivant ce lien : <a href="<?php echo $donnees['lien']; ?>">lien</a><br />
-			 Description du produit : <?php echo $donnees['description']; ?><br />
-			 Photo du produit : <img src="<?php echo $donnees['photo']; ?>"><br />
-			</p>
+			<div class="card text-center" style="width: 18rem;">
+				<div class="card-header">			
+			 Modèle <?php echo $donnees['nom_produit']; ?>
+			 	</div>
+			
+			 	
+
+			Photo du produit : <img class=" card-img-top formpic" src="<?php echo $donnees['photo']; ?>"><br />
+
+			 De type <?php echo $donnees['type']; ?><br />
+
+			 Revendeurs : <a  class=" btn btn-custom2 btn-xs lienproduit" href="<?php echo $donnees['lien']; ?>">Shop</a><br />
+
+
+			 Description: <?php echo $donnees['description']; ?><br />
+			
+			</div>
 		<?php
 		}
 
 		?>
+			</div> <!--FIN col-md-8-->
+		</form>
+
+
+	
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-										<!--  FOOTER -->
+	</div> <!--FIN ROW-->
+</div> <!-- FIN CONTAINER -->
+									<!--  FOOTER -->
 
 <footer>
 	<? include('footer_view.php') ?>
 </footer>
-<script src="js/bootstrap.js"></script>
+
 </body>
 </html>
